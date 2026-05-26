299a3997b95b673ce45247802808e952e0f921ca
rainbownash <anastasie.wagenheim@gmail.com>
2026-05-26 11:33:44 +0200

    Create download_dataset.py

---
+++ download_dataset.py
@@ -0,0 +1,25 @@
+import gdown
+import zipfile
+import os
+
+DATA_URL = "https://drive.google.com/uc?id=1SaVTyD5K5nV0KlDGlLOHmUFp5AKHHACz"
+
+ZIP_NAME = "dataset.zip"
+OUTPUT_DIR = "dataset"
+
+def download_dataset():
+    print("📦 Descargando dataset...")
+
+    gdown.download(DATA_URL, ZIP_NAME, quiet=False)
+
+    print("📂 Descomprimiendo dataset...")
+
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    with zipfile.ZipFile(ZIP_NAME, 'r') as zip_ref:
+        zip_ref.extractall(OUTPUT_DIR)
+
+    print(f"✅ Dataset listo en: {OUTPUT_DIR}")
+
+if __name__ == "__main__":
+    download_dataset()
